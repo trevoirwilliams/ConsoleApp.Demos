6da1381b69f6d3f608154a55e64ebc101c54acbc
trevoirwilliams <braniac22@hotmail.com>
2023-12-14 15:20:28 -0500

    Added Operators demo and enhanced user input demo.

---
+++ ConsoleApp.Operators/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+/* 
+    Math Operations and operators
+ */
+// Add numbers
+int sum = num1 + num2;
+// multiply
+int product = num1 * num2;
+// Division
+int quotient = num1 / num2;
+// Subtraction
+int difference = num1 - num2;
+// Modulus
+int mod = num1 % num2;
+
+Console.WriteLine("***************** Math Results **************");
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+Console.WriteLine($"Modulus: {mod}");
+Console.WriteLine();
+Console.WriteLine("***************** End Math Results **************");
+
+
+/*
+    Logic Operations and Operators
+ */
+
+bool isGreaterThan = num1 > num2;
+bool isLessThan = num1 < num2;
+bool isEqualTo = num1 == num2;
+bool isGreaterThanOrEqualTo =  num1 >= num2;
+bool isLessThanOrEqualTo =  num1 <= num2;
+bool isNotEqual =  num1 != num2;
+
+Console.WriteLine("***************** Logic Results **************");
+Console.WriteLine();
+Console.WriteLine($"Is Greater Than: {isGreaterThan}");
+Console.WriteLine($"Is Less Than: {isLessThan}");
+Console.WriteLine($"Is Equal To: {isEqualTo}");
+Console.WriteLine($"Is Greater Than Or Equal To: {isGreaterThanOrEqualTo}");
+Console.WriteLine($"Is Less Than Or Equal To: {isLessThanOrEqualTo}");
+Console.WriteLine($"Is Not Equal: {isNotEqual}");
+Console.WriteLine();
+Console.WriteLine("***************** End Logic Results **************");
+
+/*
+    Assignment Operations and Operators
+ */
+Console.Write("Enter Random Value for Assignment Operations: ");
+int randomValue = Convert.ToInt32(Console.ReadLine());
+//int increaseValue = num1 + randomValue;
+
+Console.WriteLine("***************** Assignment Results **************");
+Console.WriteLine();
+//num1 = num1 + randomValue;
+num1 += randomValue;
+Console.WriteLine($"Num 1 increased by {randomValue}: {num1}");
+//num1 = num1 - randomValue;
+num1 -= randomValue;
+Console.WriteLine($"Num 1 reduced by {randomValue}: {num1}");
+//num1 = num1 * randomValue;
+num1 /= randomValue;
+Console.WriteLine($"Num 1 divided by {randomValue}: {num1}");
+//num1 = num1 / randomValue;
+num1 %= randomValue;
+Console.WriteLine($"Num 1 mod by {randomValue}: {num1}");
+//num1 = num1 % randomValue;
+num1 *= randomValue;
+Console.WriteLine($"Num 1 multiplied by {randomValue}: {num1}");
+Console.WriteLine();
+Console.WriteLine("***************** End Assignment Results **************");
--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,20 +1,31 @@
-﻿// Declare variables
+﻿//Variable Declarations and Types
 
-string firstName = string.Empty;
+string? firstName = string.Empty;
 string lastName = string.Empty;
-int age = 0;
+int age;
 int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool working = true;
 
 // Prompt the user for input
-Console.WriteLine("Please enter your first name");
+Console.Write("Please enter your first name: ");
 firstName = Console.ReadLine();
 
-Console.WriteLine("Please enter your last name");
+Console.Write("Please enter your last name: ");
 lastName = Console.ReadLine();
 
-
-Console.WriteLine("Please enter your age");
+Console.Write("Please enter your age: ");
 age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter your salary: "); 
+salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Please enter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you working? (true or false): ");
+working = Convert.ToBoolean(Console.ReadLine());
 
 // Process the data 
 int workingYearsRemaining = retirementAge - age;
@@ -22,4 +33,7 @@ int workingYearsRemaining = retirementAge - age;
 // Output the results to the user
 Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
-Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+Console.WriteLine($"Your Salary is: {salary}"); 
+Console.WriteLine($"Your Gender is: {gender}"); 
+Console.WriteLine($"You Are Employed: {working}");
+Console.WriteLine($"Number of working years remaining: {workingYearsRemaining}");
