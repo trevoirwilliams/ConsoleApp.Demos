708f84c25bed8942f3cb167cc690744a2ab113b8
trevoirwilliams <braniac22@hotmail.com>
2024-01-03 14:31:04 -0500

    Added methods and modified the calculator

---
+++ ConsoleApp.Methods/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("************* - Methods - *************");
+
+// void methods - complete a task without returning a value
+void PrintName()
+{
+    // Method code
+    Console.WriteLine("Trevoir");
+}
+
+// value returning methods - returns a value after an operation
+int GetFiveYearsAgo()
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+    return year;
+}
+
+
+// methods with parameters - represent data being passed into a method.
+void PrintNameWithParams(string name)
+{
+    // Method code
+    Console.WriteLine("You name is: " + name);
+}
+
+int GetYearDifferenceWithParams(int year)
+{
+    int yearDifference = DateTime.Now.Year - year;
+    return yearDifference;
+}
+// methods with optional parameters - parameter is not required..also has a default value
+int GetFutureOrPastYear(int numberOfYears = 0)
+{
+    var year = DateTime.Now.AddYears(numberOfYears).Year;
+    return year;
+}
+
+// methods with nullable parameters
+void PrintNameNullableParam(string? name, int? count)
+{
+//if (string.IsNullOrEmpty(name))
+//{
+//    name = "Default Name";
+//}
+
+//if(!count.HasValue)
+//{
+//    count = 1;
+//}
+
+// The null-coalescing assignment operator ??= assigns the value of its right-hand operand to its left-hand operand only if the left-hand operand evaluates to null.
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
+    name ??= "Default Name";
+    count ??= 1;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(name);
+    }
+}
+
+/* Function Calls */
+PrintName();
+int fiveYearsAgo = GetFiveYearsAgo();
+Console.WriteLine("Five Years Ago was: " + fiveYearsAgo);
+
+Console.WriteLine("Enter your name");
+string name1 = Console.ReadLine();
+PrintNameWithParams(name1);
+
+Console.WriteLine("Enter a year");
+int pastYear = Convert.ToInt32(Console.ReadLine());
+int yearsAgo = GetYearDifferenceWithParams(pastYear);
+Console.WriteLine("This was " + yearsAgo + " years ago");
+
+Console.WriteLine("Enter number of years in the future or past");
+int numberOfYears = Convert.ToInt32(Console.ReadLine());
+
+var pastYear1 = GetFutureOrPastYear();
+Console.WriteLine("The year is: " + pastYear1);
+
+var pastYear2 = GetFutureOrPastYear(numberOfYears);
+Console.WriteLine("The year is: " + pastYear2);
+
+PrintNameNullableParam(null, null);
+PrintNameNullableParam("Chester", 5);
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -10,14 +10,7 @@ while (choice != -1)
     try
     {
         // Welcome Message
-        Console.Clear();
-        Console.WriteLine("******** - Welcome to the sample calculator! - ********");
-        Console.WriteLine("Please select an operation (-1 to exit program) ");
-        Console.WriteLine("1. Addition");
-        Console.WriteLine("2. Subtraction");
-        Console.WriteLine("3. Multiplication");
-        Console.WriteLine("4. Division");
-        Console.WriteLine("5. Fibonacci sequence");
+        PrintMenu();
 
         choice = Convert.ToInt32(Console.ReadLine());
         
@@ -61,22 +54,19 @@ while (choice != -1)
         switch (choice)
         {
             case 1:
-                answer = num1 + num2;
+                answer = AddNumbers(num1, num2);
                 break;
             case 2:
-                answer = num1 - num2;
+                answer = SubtractNumbers(num1, num2);
                 break;
             case 3:
-                answer = num1 * num2;
+                answer = Product(num1, num2);
                 break;
             case 4:
-                answer = num1 / num2;
+                answer = Quotient(num1, num2);
                 break;
             case 5:
-                for (int i = num1; i <= num2; i++)
-                {
-                    answer += i;
-                }
+                answer = Fibonnaci(num1, num2);
                 break;
             default:
                 throw new Exception("Invalid Menu Item Selected.");
@@ -101,3 +91,46 @@ while (choice != -1)
 }
 
 Console.WriteLine("******** - Thank you for using the sample calculator! - ********");
+
+
+// Method Definitions
+int AddNumbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int SubtractNumbers(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int Product(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int Quotient(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+int Fibonnaci(int num1, int num2)
+{
+    var answer = 0;
+    for (int i = num1; i <= num2; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+void PrintMenu()
+{
+    Console.Clear();
+    Console.WriteLine("******** - Welcome to the sample calculator! - ********");
+    Console.WriteLine("Please select an operation (-1 to exit program) ");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Fibonacci sequence");
+}
